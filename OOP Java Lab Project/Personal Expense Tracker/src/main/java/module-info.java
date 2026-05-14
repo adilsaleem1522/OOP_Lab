f8452269a4eb3109c7d6cb948419c259194f29b2
Muhammad Adil Saleem <adilsaleem1522@gmail.com>
2026-05-14 17:25:11 +0500

    Remove OOP project from GitHub

--- OOP Java Lab Project/Personal Expense Tracker/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module user.org.personal_expense_tracker {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens user.org.personal_expense_tracker to javafx.fxml;
-    exports user.org.personal_expense_tracker;
-}
--- OOP Java Lab Project/Personal Expense Tracker/src/main/java/user/org/personal_expense_tracker/Credentials.java
+++
@@ -1,8 +0,0 @@
-package user.org.personal_expense_tracker;
-
-import java.util.ArrayList;
-
-public class Credentials {
-    public static ArrayList<UserData> users = new ArrayList<>();
-    public static UserData currentUser = null;
-}
